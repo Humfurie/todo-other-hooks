dd4f2058da67060ecd3afa178fe7eadddb9d5081
Humfurie <humfurie@gmail.com>
2022-11-03 16:34:40 +0800

    chore: updating reducer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,76 @@
-import { stringify } from "querystring"
 import { useReducer } from "react"
 export default function Home() {
 
-  const ACTION ={
-    ADD_TODO: "add_todo"
+  
+  const ACTION = {
+    ADD_TODO: 'add_todo',
+    CHANGE_TODO: 'change_todo'
   }
-  const reducer = (state: any, action: any) => {
-    switch (action.type) {
-      case ACTION.ADD_TODO:
-        return { ...state, userInput: action.payload}
+  
+  type stateType = {
+    todo: string[],
+    userInput: string,
+  }
 
+  type actionType = {
+    type: string,
+    payload: string,
+  }
+  
+  const initialValue:stateType = {
+    todo: [],
+    userInput: ''
+  }
+
+  const reducer = (state: stateType, action: actionType) => {
+    console.log(state.userInput, "data")
+    console.log(state.todo, "todo")
+    switch (action.type){
+      case ACTION.ADD_TODO:
+        return { 
+          ...state, 
+          todo:[...state.todo, action.payload] }
+      case ACTION.CHANGE_TODO:
+        return {
+          ...state, 
+          userInput: action.payload }
+      default:
+        return state
     }
   }
-  const initialState = {
-    userInput: {name: '', status: false, show: false},
-    todoList: [{name: '', status: false, show: false}],
-    cloneTodoList: []
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    dispatch({type: ACTION.ADD_TODO, payload: e.target.value})
   }
-  const [state, dispatch] = useReducer<any>(reducer, initialState)
+
+  const handleOnchange = (e: any) => {
+    // console.log(state.todo)
+    dispatch({type: ACTION.CHANGE_TODO, payload: e.target.value})
+  }
+ const [state, dispatch] = useReducer<(args: any, payload: any) => any >(reducer, initialValue )
   return (
     <div>
-
-        <input type="text" onChange={(e) => {
-          dispatch({ type: ACTION.ADD_TODO , payload: e.target.value })
-        }} />
-
+      <form>
+        <input 
+        type="text"
+        name="userInput"
+        onChange={(e) => {
+          e.preventDefault()
+          handleOnchange(e)
+        }}/>
+        <button
+        onClick={(e) => {
+          e.preventDefault()
+          handleSubmit(e)  
+        }}>
+          submit
+        </button>
+      </form>
+      <div>
+        {state.todo}
+        {state.userInput}
+        </div>
     </div>
   )
 }
